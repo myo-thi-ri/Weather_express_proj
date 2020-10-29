49ec43a78f5b1f1b4ffed6adef6b5915ee024318
MYO THIRI <sandarlamin.llm@gmail.com>
2020-10-30 02:19:24 +0900

    Change the unit of temp.

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ app.get('/',function(req,res){
 
 app.post('/', function (req, res) {
     const query = req.body.cityName;
-    const apiKey = process.env.API_KEY;
-    const unit = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units=unit"
+    const apiKey = "4fc145a29e5e0b7f8bb3055a335e63cc";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units=imperial"
     
     https.get(url,function(response){
         console.log(response.statusCode);
 
         response.on('data',function(data){
+            //console.log(data);
             const weatherdata = JSON.parse(data);
-            //console.log(weatherdata);
+            console.log(weatherdata);
             const name = weatherdata.name;
             const temp = weatherdata.main.temp;
-            const description = weatherdata.weather[0].description;
+            const description = weatherdata.weather[0].main;
             const icon = weatherdata.weather[0].icon;
             const mintemp = weatherdata.main.temp_min;
             const maxtemp = weatherdata.main.temp_max;
