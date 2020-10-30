bcf37325c42d3a1f26b29fbf22e8465d87dbd4f8
MYO THIRI <sandarlamin.llm@gmail.com>
2020-10-30 16:03:39 +0900

    change css file.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get('/',function(req,res){
